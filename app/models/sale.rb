eafbc60a02fea2f190bafd789c2a11b7142f0ea3
ChunyuBai <baichunyu26@gmail.com>
2023-01-29 11:50:54 -0500

    refactor status for sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,14 @@
 class Sale < ApplicationRecord
+  def finished?
+    self.ends_on < Date.current
+  end
+
+  def upcoming?
+    self.starts_on > Date.current
+  end
+
+  def active?
+    !finished? && !upcoming? 
+  end
+
 end
